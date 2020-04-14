6e3b419f1ada1a0b34dd5ce615737b40f06d6636
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2020-04-13 21:26:33 -0300

    Initial project setup

---
+++ PushCore/PushCore.h
@@ -0,0 +1,19 @@
+//
+//  PushCore.h
+//  PushCore
+//
+//  Created by Ricardo Bailoni on 13/04/20.
+//  Copyright © 2020 Ricardo Bailoni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PushCore.
+FOUNDATION_EXPORT double PushCoreVersionNumber;
+
+//! Project version string for PushCore.
+FOUNDATION_EXPORT const unsigned char PushCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PushCore/PublicHeader.h>
+
+
